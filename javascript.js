089d4fca0edf51e4988ed2555eb7b46c7d0319e1
tyggedame <afl@outlook.dk>
2022-12-06 13:34:21 +0100

    add

--- javascript.js
+++ javascript.js
@@ -2,32 +2,22 @@
 
 
 
-//let messageDiv = document.getElementById("message")
-
-// console.log(messageDiv)
-
  let html
 
  html = "<h1>"
  html += "Skønt "
-//html += urlObject.searchParams.get("your-name")
+
  html += "</h1>"
 
  html += "<p>"
  html += " Tak for din tilmelding ❤️‍🔥"
  html += "</p>"
- html += "Glæd dig, du hører fra mig i din mail. "
- //html += urlObject.searchParams.get("your-email")
+ html += "Glæd dig, -du hører fra mig i din mail. "
+ 
  html += "</p>"
 
- //messageDiv.innerHTML = html 
 
-
- let backBtn = document.getElementById("back-btn")
-
- //backBtn.onclick = function (){
-   //  window.location = "nyhedsbrev.html"
-    //}
+ 
 
     const submit = document.querySelector(".c-form")
     submit.addEventListener("submit", submitHandler)
