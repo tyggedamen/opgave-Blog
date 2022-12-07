8e6625b680f464f88babf801f982a3eafa3fee0e
tyggedame <afl@outlook.dk>
2022-12-07 11:34:16 +0100

    new

--- javascript.js
+++ javascript.js
@@ -2,28 +2,28 @@
 
 
 
- let html
+let html
 
- html = "<h1>"
- html += "Skønt "
+html = "<h1>"
+html += "Skønt "
 
- html += "</h1>"
+html += "</h1>"
 
- html += "<p>"
- html += " Tak for din tilmelding ❤️‍🔥"
- html += "</p>"
- html += "Glæd dig, -du hører fra mig i din mail. "
- 
- html += "</p>"
+html += "<p>"
+html += " Tak for din tilmelding ❤️‍🔥"
+html += "</p>"
+html += "Glæd dig, -du hører fra mig i din mail. "
+
+html += "</p>"
 
 
- let backBtn = document.getElementById("back-btn")
+let backBtn = document.getElementById("back-btn")
 
-    const submit = document.querySelector(".c-form")
+const submitBtn = document.querySelector(".c-form")
 
-    submit.addEventListener("submit", submitHandler)
+submitBtn.addEventListener("submit", submitHandler)
 
-    function submitHandler(e){
-        e.preventDefault()
-        document.querySelector(".c-form").innerHTML = html
-    }
+function submitHandler(e) {
+    e.preventDefault()
+    document.querySelector(".c-form").innerHTML = html
+}
